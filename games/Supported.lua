64060b78163c3568451580f2db2eb1696bf74ca1
hayden-droid <61548447+hayden-droid@users.noreply.github.com>
2021-07-30 19:55:11 +0100

    Create Supported.lua

---
+++ games/Supported.lua
@@ -0,0 +1,17 @@
+local t = {
+    [4872321990] = "Islands",
+    [5899156129] = "Islands",
+    [1962086868] = "TowerOfHell",
+    [537413528] = "BuildABoat",
+    [5041144419] = "SCP Roleplay",
+    [2041312716] = "Ragdoll Engine",
+    [189707] = "Natural Disaster",
+    [286090429] = "Arsenal",
+    [648362523] = "Breaking Point",
+    [2248408710] = "DestructionSimulator",
+    [730951264] = "The Maze",
+    [263761432] = "HorrificHousing",
+    [292439477] = "Phantom Forces"
+}
+
+return t
